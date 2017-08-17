6a2df9d71d3cdfb13fa1d1bbb56b5fed3919d972
xu.mingxun <908581016@qq.com>
2017-08-17 09:30:38 +0800

    add

--- learn.py
+++ learn.py
@@ -1,19 +1,55 @@
 from record import Record
-# from keras.models import Sequential
-# from keras.layers import Dense
-# from keras.layers.recurrent import LSTM
-
+from keras.models import Sequential
+from keras.layers import Dense,Flatten,Reshape
+from keras.layers.recurrent import LSTM
+import random
 
 class Learn:
     record=None
+
+
     def learn(self):
         self.record=Record()
+        random.seed(1337)
 
-        x,y=self.record.load(0,20)
-        print(x.shape)
-        print(y.shape)
+        model=Sequential()
+
+        model.add(LSTM(
+            256,
+            input_shape=(225,225),
+            activation='sigmoid',
+            return_sequences=False
+        ))
+        model.add(Reshape((-1,)))
+        model.add(Dense(1024, activation='sigmoid'))
+        model.add(Dense(256,activation='sigmoid'))
+        model.add(Dense(64, activation='sigmoid'))
+        model.add(Dense(16, activation='sigmoid'))
+        model.add(Dense(1, activation='tanh'))
+        model.compile(optimizer='adam',loss='mse',metrics=['accuracy'])
+
+        model.load_weights("weights.h5")
+
+        x,y=self.record.load(20,10)
+        pred_y=model.predict(x)
+        print(y)
+        print(pred_y)
+
+        '''
+        print(model.summary())
+
+        for i in range(100):
+            print('roll in ', i)
+            batch_size=100
+            x, y = self.record.load(i, int(batch_size/2))
+            model.fit(x,y,batch_size=batch_size,epochs=10,verbose=1)
+            model.save_weights("weights.h5")'''
+
+
         # print(x)
         # (100, 225, 15, 15)
+
+
 
 
 l=Learn()
--- record.py
+++ record.py
@@ -22,6 +22,23 @@ class Record:
         else:
             self.db.update('update chess set num=num+1 where md5=%s',md5)
 
+    '''
+    def load(self,pageNo=0,pageSize=1):
+        datas = self.db.query("select * from chess order by learn_num asc,id asc limit " + str(pageNo * pageSize) + "," + str(pageSize))
+        data=datas[0]
+
+        one = []
+        board = [[0 for i in range(15)] for j in range(15)]
+        steps = json.loads(data[1])
+        for step in steps:
+            board[step[0]][step[1]] = step[2]
+            one.append(board.copy())
+        for step in range(225 - len(steps)):
+            one.append([[0 for i in range(15)] for j in range(15)])
+
+        return np.array(one),np.array(data[4])
+    '''
+    ''''''
     def load(self,pageNo=0,pageSize=100):
         datas=self.db.query("select * from chess order by learn_num asc,id asc limit "+str(pageNo*pageSize)+","+str(pageSize))
 
@@ -29,13 +46,13 @@ class Record:
         y=[]
         for row in datas:
             one=[]
-            board=[[0 for i in range(15)] for j in range(15)]
+            board=[0 for i in range(225)]
             steps=json.loads(row[1])
             for step in steps:
-                board[step[0]][step[1]]=step[2]
+                board[step[0]*15+step[1]]=step[2]
                 one.append(board.copy())
             for step in range(225-len(steps)):
-                one.append([[0 for i in range(15)] for j in range(15)])
+                one.append([0 for i in range(225)])
 
             result.append(one)
             y.append(row[4])
