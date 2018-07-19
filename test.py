ad2f52236119fe2c0fbb423cd3032a6ef3a27d27
xu.mingxun <xu.mingxun@puscene.com>
2018-07-19 18:58:06 +0800

    add

--- test.py
+++ test.py
@@ -19,17 +19,17 @@ activate=None
 
 model=Sequential()
 model.add(Reshape(input_shape=(8,5),target_shape=(8,5,1)))
-model.add(Conv2D(16,kernel_size=5,strides=1,padding='same',use_bias=False,activation=None))
+model.add(Conv2D(16,kernel_size=3,strides=1,padding='same',use_bias=False,activation=None))
 model.add(Conv2D(32,kernel_size=5,strides=1,padding='same',use_bias=False,activation='relu'))
 #
 model.add(BatchNormalization())
-model.add(Conv2D(64,kernel_size=5,strides=1,padding='same',use_bias=False,activation='relu'))
 model.add(Conv2D(64,kernel_size=3,strides=1,padding='same',use_bias=False,activation='relu'))
+model.add(Conv2D(64,kernel_size=5,strides=1,padding='same',use_bias=False,activation='relu'))
 model.add(Conv2D(64,kernel_size=1,strides=1,padding='same',use_bias=False,activation='relu'))
 
 model.add(BatchNormalization())
-model.add(Conv2D(64,kernel_size=5,strides=1,padding='same',use_bias=False,activation='relu'))
 model.add(Conv2D(64,kernel_size=3,strides=1,padding='same',use_bias=False,activation='relu'))
+model.add(Conv2D(64,kernel_size=5,strides=1,padding='same',use_bias=False,activation='relu'))
 model.add(Conv2D(64,kernel_size=1,strides=1,padding='same',use_bias=False,activation='relu'))
 
 model.add(Flatten())
